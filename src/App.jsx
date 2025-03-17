73b920f1ae76311b22d767b2a622f6fbe26a10f8
Claire <c.tregunna1980@gmail.com>
2025-03-17 17:58:04 +0000

    added in state for button color change

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Header from './Header.jsx'
 import Nav from './Nav.jsx'
 
@@ -42,23 +43,43 @@ function getPriority(priority) {
 }
 
 function TaskLisk(props) {
+  const [color, setColor] = useState('bg-gray-50')
+  const [text, setText] = useState('Complete')
+
+  const updateColor = () => {
+    if(color == 'bg-gray-50') {
+      setColor('bg-green-500')
+    } else {
+      setColor('bg-gray-50')
+    }
+  }
+
+  const updateText = () => {
+    if(text == 'Complete') {
+      setText("Task Completed!!")
+    } else {
+      setText("Complete")
+    }
+  }
+
   return (
-      <div className='p-4 bg-slate-900 flex flex-col rounded-md shadow-[0px_1px_6px_#000] text-white hover:scale-[1.02] hover:duration-400 hover:ease-in-out'>
-        <h3 className='text-center uppercase text-lg font-semibold mb-10'>{props.taskName}</h3>
-        <span className='mb-2'>Estimated Time: {props.timeToDo} mins</span>
+      <div className='p-4 bg-slate-900 flex flex-col justify-between rounded-md shadow-[0px_1px_6px_#000] text-white hover:scale-[1.02] hover:duration-400 hover:ease-in-out'>
+        <h3 className='text-center uppercase text-lg font-semibold mb-6'>{props.taskName}</h3>
+        <span>Estimated Time: {props.timeToDo} mins</span>
         <span>Priority: <span className={getPriority(props.priority)}>{props.priority}</span></span>
+        <button className={`${color} text-black py-2 px-4 text-xs md:w-1/2 mx-auto rounded-sm hover:cursor-pointer`} onClick={() => {updateColor(); updateText()}}>
+        {text}</button>
       </div>
   )
 }
 
 function App() {
-
   return (
     <>
       <Header />
       <div className='flex flex-col md:flex-row font-main'>
         <Nav />
-        <div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-[170px] gap-10 p-10'>
+        <div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-[200px] gap-10 p-10'>
           {tasks.map(task =>
             <TaskLisk 
               taskName={task.taskName}
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,9 +1,9 @@
 export default function Nav() {
     return (
         <nav className="w-screen md:w-50 h-10 md:h-screen bg-black text-white font-bold text-center md:pt-10 flex items-center justify-around md:flex-col md:justify-normal">
-            <div className="md:mb-6 text-lg hover:underline hover:cursor-pointer">Tasks to do</div>
-            <div className="md:mb-6 text-lg hover:underline hover:cursor-pointer">In progress</div>
-            <div className="md:mb-6 text-lg hover:underline hover:cursor-pointer">Completed</div>
+            <div className="md:mb-6 text-sm md:text-lg hover:underline hover:cursor-pointer">Tasks to do</div>
+            <div className="md:mb-6 ttext-sm md:ext-lg hover:underline hover:cursor-pointer">In progress</div>
+            <div className="md:mb-6 text-sm md:text-lg hover:underline hover:cursor-pointer">Completed</div>
         </nav>
     )
 }
